5a82ac0b3bc1605f6f9759891d08bd96d9c2fe13
hmm <60791542+hmm-hub@users.noreply.github.com>
2022-07-26 23:28:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-try {
-    app.downloadfile("https://i.ytimg.com/vi/xtkOW1xi8f8/hqdefault.jpg", "Download", "file.png");
-  } catch (Error) {
-    try {
-      app.requeststorage();
-    } catch (Error) {}
-  }
+app.downloadfile("https://i.ytimg.com/vi/xtkOW1xi8f8/hqdefault.jpg", "Download", "files.png");
+app.toast("e","short");
