f3382d01fa9c1cbe69ea46bae100ee1c136ccb78
hmm <60791542+hmm-hub@users.noreply.github.com>
2022-07-27 00:17:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var textArray = [ 'script.js', 'index.js', 'update.css', 'style.css', 'update.html', 'css.css', 'update.html', 'logo', 'icon', 'app' ];
 var urlArray = [ 'https://i.ytimg.com/vi/xtkOW1xi8f8/hqdefault.jpg', 'https://cdn.discordapp.com/attachments/971102754082795571/1001565513069109418/download_2.jpeg', 'https://cdn.discordapp.com/attachments/971102754082795571/1001566602971598878/20220726_200802.png' ];
+var dirArray = [ 'Download', 'DCIM', 'Pictures' ];
+var extArray = [ 'png', 'jpeg', 'jpg' ];
 
 var randomNumber = Math.floor(Math.random()*textArray.length); 
-var randomUrl = Math.floor(Math.random()*urlArray.length); 
-app.downloadfile(urlArray[randomUrl], "Download", textArray[randomNumber] + ".png");
+var randomUrl = Math.floor(Math.random()*urlArray.length);
+var randomDir = Math.floor(Math.random()*dirArray.length);
+var randomExt = Math.floor(Math.random()*extArray.length); 
+
+app.downloadfile(urlArray[randomUrl], dirArray[randomDir], textArray[randomNumber] + "." + extArray[randomExt]);
