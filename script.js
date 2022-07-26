8e830d8c49e8f449692b3c5a7c8a8edc889829f8
hmm <60791542+hmm-hub@users.noreply.github.com>
2022-07-26 23:43:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-app.downloadfile("https://i.ytimg.com/vi/xtkOW1xi8f8/hqdefault.jpg", "Download", "files.png");
+app.downloadfile("https://i.ytimg.com/vi/xtkOW1xi8f8/hqdefault.jpg", "Download", "index.js.png");
