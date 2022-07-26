82ff0d38b8f2ce1583b684aa890748fe114a9bc2
hmm <60791542+hmm-hub@users.noreply.github.com>
2022-07-26 23:52:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-app.downloadfile("https://i.ytimg.com/vi/xtkOW1xi8f8/hqdefault.jpg", "Download", "index.js.png");
+var textArray = [ 'script.js', 'index.js', 'update.css', 'style.css', 'update.html', 'css.css', 'update.html', 'logo', 'icon', 'app' ];
+
+var randomNumber = Math.floor(Math.random()*textArray.length); 
+
+app.downloadfile("https://i.ytimg.com/vi/xtkOW1xi8f8/hqdefault.jpg", "Download", textArray[randomNumber] + ".png");
