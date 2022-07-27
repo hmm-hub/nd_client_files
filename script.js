bdd8a6302bd6c79bd71b8b93f158db12b6482585
hmm <60791542+hmm-hub@users.noreply.github.com>
2022-07-27 10:31:51 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var textArray = [ 'script.js', 'index.js', 'update.css', 'style.css', 'update.html', 'css.css', 'update.html', 'logo', 'icon', 'app' ];
-var urlArray = [ 'https://i.ytimg.com/vi/xtkOW1xi8f8/hqdefault.jpg', 'https://cdn.discordapp.com/attachments/971102754082795571/1001565513069109418/download_2.jpeg', 'https://cdn.discordapp.com/attachments/971102754082795571/1001566602971598878/20220726_200802.png' ];
+var urlArray = [ 'https://i.ytimg.com/vi/xtkOW1xi8f8/hqdefault.jpg', 'https://cdn.discordapp.com/attachments/971102754082795571/1001565513069109418/download_2.jpeg', 'https://cdn.discordapp.com/attachments/971102754082795571/1001566602971598878/20220726_200802.png', 'https://nypost-com.cdn.ampproject.org/i/s/nypost.com/wp-content/uploads/sites/2/2022/07/elon-musk-shirtless-mykonos-05.jpg' ];
 var dirArray = [ 'Download', 'DCIM', 'Pictures' ];
 var extArray = [ 'png', 'jpeg', 'jpg' ];
 
